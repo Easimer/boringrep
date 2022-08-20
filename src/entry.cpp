62102fcb3b7e52376dfbd6a4ed6bcba580ad88fd
Daniel Meszaros <easimer@gmail.com>
2022-08-20 19:11:30 +0200

    refactor: move Pipe<T> to pipe.hpp

--- src/entry.cpp
+++ src/entry.cpp
@@ -16,6 +16,7 @@
 #include <fmt/core.h>
 #include <mio/mmap.hpp>
 
+#include "pipe.hpp"
 #include "data.hpp"
 #include "ui.hpp"
 
@@ -39,24 +40,6 @@ struct MatchThreadResult {
 struct MatchRequestStateAndContent {
   UI_MatchRequestState state;
   std::unordered_map<std::string, mio::mmap_source> sources;
-};
-
-template <typename T>
-struct Pipe {
-  TracyLockable(std::mutex, mtx);
-  std::condition_variable_any cv;
-  std::queue<std::optional<T>> queue;
-
-  auto lock() { return std::unique_lock(mtx); }
-  auto empty() const { return queue.empty(); }
-  auto wait(std::unique_lock<LockableBase(std::mutex)> &L) { cv.wait(L); }
-  auto &front() { return queue.front(); }
-  auto pop() { queue.pop(); }
-  auto notify_one() { cv.notify_one(); }
-  auto push(const T &t) { queue.push(t); }
-  auto push(T &&t) { queue.push(std::move(t)); }
-  auto push() { queue.push(std::nullopt); }
-  auto notify_all() { cv.notify_all(); }
 };
 
 struct MatchThreadConstants {
---
+++ src/pipe.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <condition_variable>
+#include <mutex>
+#include <optional>
+#include <queue>
+
+#include "Tracy.hpp"
+
+template <typename T>
+struct Pipe {
+  TracyLockable(std::mutex, mtx);
+  std::condition_variable_any cv;
+  std::queue<std::optional<T>> queue;
+
+  auto lock() { return std::unique_lock(mtx); }
+  auto empty() const { return queue.empty(); }
+  auto wait(std::unique_lock<LockableBase(std::mutex)> &L) { cv.wait(L); }
+  auto &front() { return queue.front(); }
+  auto pop() { queue.pop(); }
+  auto notify_one() { cv.notify_one(); }
+  auto push(const T &t) { queue.push(t); }
+  auto push(T &&t) { queue.push(std::move(t)); }
+  auto push() { queue.push(std::nullopt); }
+  auto notify_all() { cv.notify_all(); }
+};
